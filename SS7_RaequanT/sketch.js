bffbb2fcbd1131dfc5165d56ece60a016394c5cb
rtyson0 <tysonraequan@gmail.com>
2025-11-21 10:07:30 -0500

    Simple Idea

---
+++ SS7_RaequanT/sketch.js
@@ -0,0 +1,28 @@
+
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(200,230,250);
+  strokeWeight(0)
+
+  fill(150,50,50)
+  rect(0,340,800,130)
+
+  fill(100,200,200)
+  rect(0,470,800,330)
+
+  for(var structure = 5; structure <= width; structure = structure + 50){
+    fill(200,100,100)
+    rect(structure, 350, 40, 20)
+    rect(structure, 380, 40, 20)
+    rect(structure, 410, 40, 20)
+    rect(structure, 440, 40, 20)
+  }
+
+  fill(200)
+  rect(0,320,800,30)
+
+}
